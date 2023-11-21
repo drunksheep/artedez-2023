8fd5ebdfbbd85541ccd44412c50c4071b295a0cd
André <andre.medeiros@jotacom.com.br>
2023-11-21 15:08:31 -0300

    chore: initial cleanup and fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,15 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      fontFamily: {
+        cera: ['var(--font-cera)'],
+        marker: ['var(--font-marker)'], 
+        neue: ['var(--font-neue)'],
+      }
     },
   },
   plugins: [],
